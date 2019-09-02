e08a4ce247cc6c296a412dc1e9b1ec1fd37f068a
mek12 <49874601+mek12@users.noreply.github.com>
2019-09-02 16:49:55 +0300

    SnakeGamesProject

---
+++ Assets/apple.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class apple : MonoBehaviour {
+
+    public Text skor_txt;
+    int skor = 0;
+
+
+    movement generate_tile;
+    private void Start()
+    {
+        generate_tile = GameObject.Find("bas").GetComponent<movement>();
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name=="bas")
+        {
+            skor = +10;
+            skor_txt.text = "SKOR" + skor;
+
+            replace_coordinate();
+
+            generate_tile.extra_tile();
+
+        }
+        if (other.gameObject.tag == "tile")
+        {
+           
+
+            replace_coordinate();
+
+        }
+    }
+    void replace_coordinate()
+    {
+        float x_value = Random.Range(-20.0f, 17.0f);
+        float z_value = Random.Range(0f, +20.0f);
+        transform.position = new Vector3(x_value, 0, z_value);
+    }
+  
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movement : MonoBehaviour {
+
+    public GameObject Tile;
+    List<GameObject> Tiles;
+    Vector3 old_coordinate;
+    GameObject old_tile;
+    float speed = 15.0f;
+
+    public GameObject finish_pnl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="engel")
+        {
+            finish_pnl.SetActive(true);
+            Time.timeScale = 0.0f;
+        }
+    }
+
+    public void try_again()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("lvl1");
+    }
+
+
+    void Start ()
+    {
+        Tiles = new List<GameObject>();
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject new_tile = Instantiate(Tile, transform.position, Quaternion.identity);
+            Tiles.Add(new_tile);
+            
+        }
+        InvokeRepeating("run", 0.0f, 0.1f);
+
+    }
+ 
+    void run()
+    {
+        old_coordinate = transform.position;
+        transform.Translate(0, 0, speed * Time.deltaTime);
+        tail_tracking();
+    }
+
+    void tail_tracking()
+    {
+        Tiles[0].transform.position = old_coordinate;
+        old_tile = Tiles[0];
+        Tiles.RemoveAt(0);
+        Tiles.Add(old_tile);
+
+    }
+    public void turn(float angle)
+    {
+        transform.Rotate(0, angle, 0);
+    }
+
+    public void extra_tile()
+    {
+        GameObject new_tile = Instantiate(Tile, transform.position, Quaternion.identity);
+        Tiles.Add(new_tile);
+    }
+}
